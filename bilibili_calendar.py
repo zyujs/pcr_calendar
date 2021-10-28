9a091cadf3f4eaa1ba409029024a95fe73cc4ed6
zyujs <zyujs@msn.com>
2021-10-28 12:01:37 +0800

    fix cn end date

--- bilibili_calendar.py
+++ bilibili_calendar.py
@@ -1,3 +1,4 @@
+from os import truncate
 import re
 import ast
 from html.parser import HTMLParser
@@ -15,10 +16,10 @@ keyword_list = [
 ]
 
 event_keyword_list = [
-    'qdhd',
-    'tbhd',
-    'jqhd',
-    'tdz',
+    'qdhd', #庆典活动 04:59
+    'tbhd', #特别活动 23:59
+    'jqhd', #剧情活动 23:59
+    'tdz',  #团队战 23:59
 ]
 
 
@@ -41,11 +42,13 @@ class ContentParse(HTMLParser):
 
 def parse_content(day_content):
     content_html = ''
+    data = {}
     for keyword in event_keyword_list:
-        content_html += day_content[keyword]
-    parser = ContentParse()
-    parser.feed(content_html)
-    return parser.data
+        content_html = day_content[keyword]
+        parser = ContentParse()
+        parser.feed(content_html)
+        data[keyword] = parser.data
+    return data
 
 
 def extract_calendar_data(js_text):
@@ -67,24 +70,33 @@ def transform_calendar_data(data):
     event_list = []
     for i in range(len(data)):
         for day_str in data[i]['day']:
-            #print(data[i]['year'], data[i]['month'], day, data[i]['day'][day])
+            #print(data[i]['year'], data[i]['month'], day_str, data[i]['day'][day_str])
             # 遍历本日活动
             year = int(data[i]['year'])
             month = int(data[i]['month'])
             day = int(day_str)
-            for event_name in data[i]['day'][day_str]:
-                if event_name not in event_cache.keys():
-                    #event_cache[event_name] = {'year': year, 'month': month, 'day': int(day)}
-                    event_cache[event_name] = {
-                        'start_year': year,
-                        'start_month': month,
-                        'start_day': day,
-                        'end_year': year,
-                        'end_month': month,
-                        'end_day': day,
-                    }
+            for keyword in event_keyword_list:
+                end_time = '23:59'
+                if keyword == 'qdhd':
+                    end_time = '04:59'
+                for event_name in data[i]['day'][day_str][keyword]:
+                    if event_name not in event_cache.keys():
+                        #event_cache[event_name] = {'year': year, 'month': month, 'day': int(day)}
+                        event_cache[event_name] = {
+                            'start_year': year,
+                            'start_month': month,
+                            'start_day': day,
+                            'end_year': year,
+                            'end_month': month,
+                            'end_day': day,
+                            'end_time': end_time,
+                        }
             for event_name in list(event_cache.keys()):
-                if event_name in data[i]['day'][day_str]:
+                is_active = False 
+                for keyword in event_keyword_list:
+                    if event_name in data[i]['day'][day_str][keyword]:
+                        is_active = True
+                if is_active:
                     event_cache[event_name]['end_year'] = year
                     event_cache[event_name]['end_month'] = month
                     event_cache[event_name]['end_day'] = day
@@ -92,7 +104,7 @@ def transform_calendar_data(data):
                     event_list.append({
                         'title': event_name,
                         'start': f'{event_cache[event_name]["start_year"]}/{event_cache[event_name]["start_month"]}/{event_cache[event_name]["start_day"]} 05:00',
-                        'end': f'{event_cache[event_name]["end_year"]}/{event_cache[event_name]["end_month"]}/{event_cache[event_name]["end_day"]} 04:59',
+                        'end': f'{event_cache[event_name]["end_year"]}/{event_cache[event_name]["end_month"]}/{event_cache[event_name]["end_day"]} {event_cache[event_name]["end_time"]}',
                     })
                     event_cache.pop(event_name)
     return event_list
@@ -102,7 +114,6 @@ def transform_bilibili_calendar(data):
     data = extract_calendar_data(data)
     data = transform_calendar_data(data)
     return data
-
 
 '''
 fp = open('calendar.js', encoding='utf-8')
